c03a5ff19fc83d2b6e3fdfb9f3d67c08036a45ec
Russell Cohen <russell.r.cohen@gmail.com>
2014-10-27 23:12:45 -0700

    Basic notes and todos working without major race conditions.
    Code should certainly be cleaned up a bit, though.

--- todo.js
+++ todo.js
@@ -2,38 +2,96 @@
 
 var FireStateMixin = {
 	propTypes: {
-		firebasePointer: React.PropTypes.object.isRequired
+		firebasePointer: React.PropTypes.object.isRequired,
 	},
 
+	version: 0,
+
 	getInitialState: function() { return {
-		"hackToLetStateBeEmpty": "yup"
+		hackToLetStateBeEmpty: true,
 	}},
 
     componentDidMount: function() {
     	var self = this;
     	this.props.firebasePointer.on("value", function(snapshot) {
-    		if (self.isMounted()) {
-    			self.setState(snapshot.val());
+    		var snapshotVal = snapshot.val();
+    		var snapshotVersion = snapshotVal.version;
+    		delete snapshotVal.version;
+    		if (self.isMounted() && snapshotVersion > self.version) {
+    			console.log("updating version: ", snapshotVersion);
+    			self.version = snapshotVersion;
+    			self.needsPersist = false;
+    			
+    			self.replaceState(self.ensureAllValuesPresent(snapshotVal));
     		}
     	});
     },
 
-    componentDidUpdate: function(prevProps, prevState) {
-    	var stateToSerialize = _.pick(this.state, function(value, key) {
+    ensureAllValuesPresent: function(stateSnapshot) {
+    	var privates = this.privateVals(this.state);
+    	var withInitial = React.addons.update(this.getInitialState(), {$merge: stateSnapshot});
+    	return React.addons.update(withInitial, {$merge: privates});
+    },
+
+    privateVals: function(state) {
+    	return _.pick(state, function(value, key) {
+    		return key.indexOf("_") == 0;
+    	});	
+    },
+
+    publicVals: function(state) {
+    	return _.pick(state, function(value, key) {
     		return !key.indexOf("_") == 0;
-    	});
-		this.props.firebasePointer.set(stateToSerialize);
+    	});	
+    },
+
+    componentDidUpdate: function(prevProps, prevState) {
+
+    	if (_.isEqual(prevState, this.state)) {
+    		console.log("states equal");
+    		return;
+    	}
+
+    	if (this.readOnly) {
+    		console.log("read only")
+    		return;
+    	}
+    	if (!this.needsPersist) {
+    		this.needsPersist = true;
+    		return;
+    	}
+    	var stateToSerialize = this.publicVals(this.state);
+    	this.version += 1;
+    	var newState = React.addons.update(stateToSerialize, {version: {$set: this.version}});
+   		var self = this;
+    	console.log(newState.version, this.version);
+		this.props.firebasePointer.transaction(function(currentState) {
+			console.log("running a tx", currentState.version, newState.version);
+			if (newState.version > currentState.version) {
+				console.log("persisting", newState);
+				return newState;
+			} else {
+				console.warn("Tried to update a stale version");
+				return currentState;
+			}
+		});
+
 	},
+
+	incrementVersion: function() {
+		this.props.firebasePointer.child("version").transaction(function(version) {
+			return (version || 0) + 1;
+		})
+	}
 }
 
 var App = React.createClass({
-	mixins: [FireStateMixin],
-
+	//mixins: [FireStateMixin],
 	render: function() {
 		return <div>
 			<div className="notes-pane">
 				<NotesPane firebasePointer={this.props.firebasePointer.child("notes-pane")} />
-			</div>
+			</div> 
 			<div className="todo-pane">
 				<TodoPane firebasePointer={this.props.firebasePointer.child("todo-pane")} />
 			</div>
@@ -60,20 +118,27 @@ var TodoPane = React.createClass({
 	crossOff: function(index) {
 		var self = this;
 		return function() {
-			self.state.todos[index].done = true;
-			self.forceUpdate();
+			var indexUpdate = {}
+			indexUpdate[index] = {done: {$set: true}};
+			var newState = React.addons.update(self.state, {todos: indexUpdate});
+			console.log(newState);
+			self.setState(newState);
 		};
 	},
 
 	remove: function(index) {
 		var self = this;
 		return function() {
-			self.state.todos.splice(index, 1);
-			self.forceUpdate();
+			var withItemRemoved = React.addons.update(self.state.todos, {$splice: [[index,1]]});
+			console.log("with item removed:", withItemRemoved);
+			//self.state.todos.splice(index, 1);
+			//self.forceUpdate();
+			self.setState({todos: withItemRemoved});
 		};
 	},
 
 	render: function() {
+		//console.log("rendering todo.", this.state.todos.length);
 		var self = this;
 		var todoDivs = this.state.todos.map(function(todo, index) {
 			var className = todo.done ? "todo-done" : "";
@@ -95,6 +160,8 @@ var TodoPane = React.createClass({
 var NotesPane = React.createClass({
 
 	mixins: [FireStateMixin],
+	readOnly: true,
+
 	getInitialState: function() {
 		return {
 			notes: {},
@@ -108,6 +175,11 @@ var NotesPane = React.createClass({
 		var id = Math.random().toString(36).substring(7);
 		this.state.notes[id] = {name: name, text: ""};
 		this.setState({notes: this.state.notes, _title: "", _selectedNote: id});
+		//debugger;
+		var updatedData = {}
+		updatedData[id] = {name: name, text: "", version: 1}
+		this.props.firebasePointer.child("notes").update(updatedData);
+		this.incrementVersion();
 	},
 
 	selectNote: function(noteId) {
@@ -135,20 +207,24 @@ var NotesPane = React.createClass({
 		});
 		
 		var noteId = this.state._selectedNote;
-		var selectedNote = noteId != null ? <Note key={noteId} noteId={noteId} 
+		var selectedNote = noteId != null ? <NoteEditor key={noteId} noteId={noteId} 
 			firebasePointer={this.props.firebasePointer.child("notes/" + noteId)} /> : <div></div>;
 	
 
 		return <div>
-					<button onClick={this.addNote}>Add Note</button>
-					<input type="text" value={this.state._title} onChange={this.titleChange} />
-					<div>{notes}</div>
-					{selectedNote}
+					<div className="notes-list">
+						<button onClick={this.addNote}>Add Note</button>
+						<input type="text" value={this.state._title} onChange={this.titleChange} />
+						{notes}
+					</div>
+					<div className="note-edit">
+						{selectedNote}
+					</div>
 			   </div>;
 	}
 });
 
-var Note = React.createClass({
+var NoteEditor = React.createClass({
 	mixins: [FireStateMixin],
 	propTypes: {
 		noteId: React.PropTypes.string.isRequired
@@ -167,7 +243,7 @@ var Note = React.createClass({
 			<div>
 			Note Name: {this.state.name}
 			</div>
-			<textarea rows="4" cols="50" onChange={this.onTextUpdate} value={this.state.text}>
+			<textarea rows="4" cols="30" onChange={this.onTextUpdate} value={this.state.text}>
 		
 			</textarea>		
 		</div>;
