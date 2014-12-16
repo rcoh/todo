68456af16bdb7496be07351d7e548ab7c40ca267
Russell Cohen <russell.r.cohen@gmail.com>
2014-12-15 23:18:04 -0800

    Don't use deprecated method

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var React = require('react/addons');
 var App = require('./app');
 
 var firebasePointer = new Firebase("https://glaring-fire-654.firebaseio.com/users");
-React.renderComponent(
+React.render(
     <App firebasePointer={firebasePointer}/>,
     document.getElementById('container')
 );
