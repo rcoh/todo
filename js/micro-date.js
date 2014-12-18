33c563786f21a8943bc71f1420a8ddfeaa5d4bae
Russell Cohen <russell.r.cohen@gmail.com>
2014-12-17 23:07:59 -0800

    Chrono works with browserify now!
    No more of my simple if statement for date parsing.

--- js/micro-date.js
+++ js/micro-date.js
@@ -1,25 +1,34 @@
 var moment = require('moment');
+var chrono = require('chrono-node');
+
 var MicroDate = {
     parseNL: function(input) {
-        if (input == "today") {
-            return moment();
-        } else if (input == "eow") {
+        var special = this._specialCases(input);
+        if (special) {
+            return special;
+        }
+        var parsedDate = chrono.parseDate(input);
+        return parsedDate ? moment(parsedDate) : null;
+    },
+
+    _specialCases: function(input) {
+        if (input == "eow") {
             if (moment().day() > 5) {
                 return moment().day(12);
-            } else {
+              } else {
                 return moment().day(5);
-            }
+             }
         } else {
             return null;
         }
     },
 
     serialize: function(date) {
-        return date.format();
+        return date ? date.format() : null;
     },
 
     load: function(serialized) {
-        return moment(serialized);
+        return serialized ? moment(serialized) : null;
     },
 }
 
